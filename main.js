5cd9e63d5af9b651ad4e4c904a8925c640c3a1cb
vadivel <vadshanm@publicisgroupe.net>
2019-03-16 18:38:16 +0530

    iOS changes

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-const electron = require('electron')
+const electron = require('electron');
+//const AutoLaunch = require('auto-launch');
+
 const { app, BrowserWindow } = electron;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
+//let autoLaunch
+
 
 console.log("onload", app.isReady()) // false
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow()
+  win = new BrowserWindow({
+    setSkipTaskbar: true,
+    icon: __dirname +  '/icon.icns'
+  })
   win.setFullScreen(true);
-
   // and load the index.html of the app.
   win.loadFile('index.html')
   app.dock.show()
@@ -26,7 +32,9 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     win = null
-    console.log("Closed", app.isReady())
+    app.dock.hide()
+
+    /*console.log("Closed", app.isReady())
     app.dock.hide()
 
     if(app.isReady()){
@@ -34,7 +42,7 @@ function createWindow () {
         console.log("Inside Closed !!!")
         createWindow()
       }, 5000);
-    }
+    }*/
   })
 
   /*electron.powerMonitor.on('lock-screen', () => {
@@ -56,62 +64,74 @@ function createWindow () {
     console.log('On AC Create window !!!')
   })*/
 
-  /*win.on('minimize', function (event) {
+  win.on('minimize', function (event) {
+    app.dock.hide()
     setTimeout(() => {
       createWindow()
-    }, 5000);
-  })*/
+    }, 5000);     
+  })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
-    setTimeout(() => {
+
+  /*autoLaunch = new AutoLaunch({
+    name: 'Your app name goes here',
+  });
+  autoLaunch.isEnabled().then((isEnabled) => {
+    if (!isEnabled) autoLaunch.enable();
+  });*/
+
+  //console.log("Path", app.getPath('exe'))
+
+  setTimeout(() => {
+      createWindow()
+  }, 5000);
+  //clearTimeout(myVar);
+  //createWindow()
+  console.log("Ready", app.isReady())
+  electron.powerMonitor.on('lock-screen', () => {
+    console.log('Lock Screen Ready !!!', app.isReady())
+    if(win) {
+      win.close()
+    }
+  })
+
+  electron.powerMonitor.on('unlock-screen', () => {
+    console.log('Unlock Screen Ready !!!', app.isReady())
+    //createWindow()
+    if(app.isReady()) {
+      setTimeout(() => {
+        console.log('Inside Unlock Screen !!!')
         createWindow()
-    }, 5000);
-    //clearTimeout(myVar);
-    //createWindow()
-    console.log("Ready", app.isReady())
-    electron.powerMonitor.on('lock-screen', () => {
-      console.log('Lock Screen Ready !!!', app.isReady())
-      //app.quit()
-    })
+      }, 5000);
+    }
+  })
 
-    electron.powerMonitor.on('unlock-screen', () => {
-      console.log('Unlock Screen Ready !!!', app.isReady())
-      //createWindow()
-      if(!app.isReady()){
-        setTimeout(() => {
-          onsole.log('Inside Unlock Screen !!!')
-          createWindow()
-        }, 5000);
-      }
-    })
+  electron.powerMonitor.on('on-battery', () => {
+    console.log('On Battery Ready !!!')
+  })
 
-    electron.powerMonitor.on('on-battery', () => {
-      console.log('On Battery Ready !!!')
-    })
+  electron.powerMonitor.on('on-ac', () => {
+    console.log('On AC Ready !!!')
+  })
 
-    electron.powerMonitor.on('on-ac', () => {
-      console.log('On AC Ready !!!')
-    })
+  electron.powerMonitor.on('resume', () => {
+    console.log('On Resume Ready !!!')
+  })
 
-    electron.powerMonitor.on('resume', () => {
-      console.log('On Resume Ready !!!')
-    })
+  electron.powerMonitor.on('suspend', () => {
+    console.log('On Suspend Ready !!!')
+  })
 
-    electron.powerMonitor.on('suspend', () => {
-      console.log('On Suspend Ready !!!')
-    })
+  electron.powerMonitor.on('shutdown', () => {
+    console.log('On Shutdown Ready !!!')
+  })
 
-    electron.powerMonitor.on('shutdown', () => {
-      console.log('On Shutdown Ready !!!')
-    })
-
-    //createWindow()
-    idleStateCheck()
-
+  //createWindow()
+  idleStateCheck()
 })
 
 function idleStateCheck() {
